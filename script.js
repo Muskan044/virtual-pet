97b8614b76275542d6ee9cf10fe505151f92674a
Muskan044 <muskankhan430430@gmail.com>
2024-10-12 16:29:28 +0500

    feeding method done

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ let pet = {
     happiness: 50, // Default happiness between 0 and 100
     hunger: 50,    // Default hunger between 0 and 100
 }
+
+    // Method to feed the pet
+    feed() {
+        if (this.hunger > 0) {
+            this.hunger -= 20;
+            if (this.hunger < 0) this.hunger = 0; // Hunger can't be negative
+            alert(`${this.name} has been fed. Hunger is now: ${this.hunger}`);
+        } else {
+            alert(`${this.name} is not hungry.`);
+        }
+    },
